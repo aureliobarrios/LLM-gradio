44a4207be4f2ac6b760200b96f2bc7a661bd495f
aubarrio <aubarrios142@gmail.com>
2024-11-22 13:00:30 -0800

    first stage demo

--- app.py
+++ app.py
@@ -1,12 +1,49 @@
 import gradio as gr
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
+with gr.Blocks() as demo:
+    #build selection section
+    radio = gr.Radio(
+        ["Tutorial", "Videos", "Documentation"],
+        label="What kind of resources would you like to receive?"
+    )
+    #build chatbot interface
+    chatbot = gr.Chatbot()
+    #build textbot for message input
+    msg = gr.Textbox()
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"]
-)
+    #function to receive user input
+    def user(user_message, history):
+        return "", history + [[user_message, None]]
+    
+    #function to return bot output
+    def bot(history, radio):
+        bot_message = "You typed: " + history[-1][0] +  ", and selected: " + radio
+        history[-1][1] = bot_message
+        return history
 
-demo.launch()
+    #build button row section
+    with gr.Row():
+        clear_button = gr.Button("Clear")
+        submit_button = gr.Button("Submit")
+
+    #handle user submit
+    msg.submit(
+        user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False
+    ).then(
+        bot, [chatbot, radio], chatbot
+    )
+
+    #handle user click on clear button
+    clear_button.click(
+        lambda: None, None, chatbot, queue=False
+    )
+
+    #handle user click on submit button
+    submit_button.click(
+        user, [msg, chatbot], [msg, chatbot], queue=False
+    ).then(
+        bot, [chatbot, radio], chatbot
+    )
+
+if __name__ == "__main__":
+    demo.launch()
