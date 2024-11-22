1a0e9b2a14930334e0489484e12a2ddfcdebc614
aubarrio <aubarrios142@gmail.com>
2024-11-22 14:53:29 -0800

    html file include

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
 import gradio as gr
 
 with gr.Blocks() as demo:
+    # ---------- Components ----------
+
     #build selection section
     radio = gr.Radio(
         ["Tutorial", "Videos", "Documentation"],
+        value="Videos",
         label="What kind of resources would you like to receive?"
     )
     #build chatbot interface
-    chatbot = gr.Chatbot()
+    chatbot = gr.Chatbot(value=[
+        {"role": "assistant", "content": "Hello, how can I assist you today?"}
+    ], type="messages")
     #build textbot for message input
     msg = gr.Textbox()
-
-    #function to receive user input
-    def user(user_message, history):
-        return "", history + [[user_message, None]]
-    
-    #function to return bot output
-    def bot(history, radio):
-        bot_message = "You typed: " + history[-1][0] +  ", and selected: " + radio
-        history[-1][1] = bot_message
-        return history
 
     #build button row section
     with gr.Row():
         clear_button = gr.Button("Clear")
         submit_button = gr.Button("Submit")
 
+    #create output textbox
+    output = gr.Textbox(label="Learning Path")
+
+    # ---------- Functions ----------
+
+    #function to receive user input
+    def user(user_message, history):
+        return "", history + [{"role": "user", "content": user_message}]
+    
+    #function to return bot output
+    def bot(history, radio):
+        bot_message = "You typed: " + history[-1]["content"] +  ", and selected: " + radio
+        history.append({"role": "assistant", "content": bot_message})
+        return history
+    
+    #function to return to output text
+    def learning(history):
+        return "Built learning path for: " + history[-1]["content"]
+    
+    # ---------- Actions ----------
+
     #handle user submit
     msg.submit(
-        user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False
+        user, [msg, chatbot], [msg, chatbot], queue=False
     ).then(
         bot, [chatbot, radio], chatbot
     )
@@ -36,6 +52,8 @@ with gr.Blocks() as demo:
     #handle user click on clear button
     clear_button.click(
         lambda: None, None, chatbot, queue=False
+    ).then(
+        lambda: None, None, output
     )
 
     #handle user click on submit button
@@ -43,6 +61,8 @@ with gr.Blocks() as demo:
         user, [msg, chatbot], [msg, chatbot], queue=False
     ).then(
         bot, [chatbot, radio], chatbot
+    ).then(
+        learning, chatbot, output
     )
 
 if __name__ == "__main__":
